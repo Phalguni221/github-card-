09a5a469ec66fcf8f099431b830f5a3f1834e546
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-02-11 23:19:53 -0500

    github-card

---
+++ src/GitHubCard.js
@@ -0,0 +1,23 @@
+import './GitHubCard.css';
+import PersonalHeadshot2 from './PersonalHeadshot2.jpg'
+
+
+function GitHubCard() {
+    return (
+      <div>
+        <body>
+<h1>Github Username: Phalguni221</h1>
+          <img src={PersonalHeadshot2}/>
+          <br></br>
+          <br></br>
+          <h2>
+          I am currently an ambitious and hardworking student at the NJIT Full Stack Development Boot Camp.
+          I hope to be a future web developer and put to use the skills that I have learned over the course of the boot camp. 
+          More specifically, I want to be able to incorporate creativity with practical technical skills in the field of computer programming.
+          </h2>
+        </body>
+              </div>
+    );
+  }
+
+export default GitHubCard;
